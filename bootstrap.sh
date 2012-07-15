d4f8974211b3cd94e443c953272483240d613d8e
Nicolas Gallagher <nicolasgallagher@gmail.com>
2012-07-15 13:25:17 +0100

    Minor edit to installation script

--- bootstrap.sh
+++ bootstrap.sh
@@ -2,40 +2,48 @@
 
 cd "${HOME}/.dotfiles"
 
-# Pull down the latest changes
+# Pull down the latest changes.
 # git pull origin master
 
-# Check out submodules
+# Check out submodules.
 git submodule --quiet update --init
 
 cd "${OLDPWD}"
 
+function link() {
+    # Force create/replace the symlink.
+    ln -fs "${HOME}/.dotfiles/${1}" "${HOME}/${2}"
+}
+
 function mirrorfiles() {
-    # Copy .gitconfig
-    # Any global git commands in ~/.extra will be written to .gitconfig
-    # This prevents them being committed to the repository
+    # Copy `.gitconfig`.
+    # Any global git commands in `~/.bash_profile.local` will be written to
+    # `.gitconfig`. This prevents them being committed to the repository.
     rsync -avz --quiet ${HOME}/.dotfiles/git/gitconfig  ${HOME}/.gitconfig
 
-    # Symlink everything else
-    # bash_profile sources other files from the repository
-    # Force remove the vim directory if it is already there
+    # Force remove the vim directory if it's already there.
     if [ -e "${HOME}/.vim" ]; then
         rm -rf "${HOME}/.vim"
     fi
-    ln -fs ".dotfiles/osx"                "${HOME}/.osx"
-    ln -fs ".dotfiles/bash/bashrc"        "${HOME}/.bashrc"
-    ln -fs ".dotfiles/bash/bash_profile"  "${HOME}/.bash_profile"
-    ln -fs ".dotfiles/bash/inputrc"       "${HOME}/.inputrc"
-    ln -fs ".dotfiles/git/gitattributes"  "${HOME}/.gitattributes"
-    ln -fs ".dotfiles/git/gitignore"      "${HOME}/.gitignore"
-    ln -fs ".dotfiles/vim"                "${HOME}/.vim"
-    ln -fs ".dotfiles/vim/gvimrc"         "${HOME}/.gvimrc"
-    ln -fs ".dotfiles/vim/vimrc"          "${HOME}/.vimrc"
+
+    # Create the necessary symbolic links between the `.dotfiles` and `HOME`
+    # directory. The `bash_profile` sources other files directly from the
+    # `.dotfiles` repository.
+    link "bash/bashrc"        ".bashrc"
+    link "bash/bash_profile"  ".bash_profile"
+    link "bash/inputrc"       ".inputrc"
+    link "git/gitattributes"  ".gitattributes"
+    link "git/gitignore"      ".gitignore"
+    link "osx"                ".osx"
+    link "vim"                ".vim"
+    link "vim/gvimrc"         ".gvimrc"
+    link "vim/vimrc"          ".vimrc"
 
     echo "Dotfiles update complete"
 }
 
 read -p "This will overwrite some existing files in your home directory. Are you sure? (y/n) " -n 1
+
 echo
 if [[ $REPLY =~ ^[Yy]$ ]]; then
     mirrorfiles
